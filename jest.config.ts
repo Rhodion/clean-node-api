7a6336919e1e5309594d7846a1debdbc98da4bbb
Rhodion Souza Araujo <rhodions@gmail.com>
2020-12-21 17:17:05 -0300

    chore: add jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,8 @@ export default {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts.jest'
-  }
+  testEnvironment: 'node'
+  // transform: {
+  //   '.+\\.ts$': 'ts.jest'
+  // }
 }
