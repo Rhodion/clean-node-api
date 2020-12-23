2e7ebd0ce597d8e4f577427801143698b21a206f
Rhodion Souza Araujo <rhodions@gmail.com>
2020-12-23 10:47:07 -0300

    feat: ensure SignUpController return 400 if no name is provided

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,17 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  testEnvironment: 'node'
-  // transform: {
-  //   '.+\\.ts$': 'ts.jest'
-  // }
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 }
