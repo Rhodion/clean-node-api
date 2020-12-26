902258005c7d8bb2c337635e7c30d7e373260580
Rhodion Souza Araujo <rhodions@gmail.com>
2020-12-26 18:41:09 -0300

    feat: ensure request parse as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,4 @@
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 
 app.listen(process.env.PORT || 3001, () => {
   console.log('listening at %s', process.env.PORT || 3001)
